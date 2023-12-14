4cb7eada86899aa2ae6aece05532780e3e0fd25a
Mammad Mammadov <mmammadov194@gmail.com>
2023-12-14 22:24:17 +0400

    OK

---
+++ card-application/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar(){
+    return <nav className="">
+        <a href="/"></a>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </nav>
+}
